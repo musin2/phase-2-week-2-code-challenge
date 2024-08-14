c7d28837d9702d690536592f08c9b473ba01efe7
musin2 <muhsindaud75@gmail.com>
2024-08-14 11:33:35 +0300

    server link

--- src/Bot.jsx
+++ src/Bot.jsx
@@ -20,11 +20,11 @@ function Bot({
     //Filter Array of Your Army to remove deleted bot 
     const withoutBot = inArmy.filter((x) => x.id !== id);
     setInArmy(withoutBot);
-    fetch(`http://localhost:3000/bots/${id}`, {
+    fetch(`https://phase-2-week-2-code-challenge.onrender.com/bots/${id}`, {
       method: "DELETE",
     }).then((res) => {
       if (confirm(`Do you want to Remove Bot ${id} from the collection?`)) {
-       // window.location.reload();
+        // window.location.reload();
       }
       return res.json();
     });
